06790d888a97cf9a69d40e44b8e9e11132990e41
Lindsay <lshank017@gmail.com>
2023-08-07 08:27:13 -0500

    added to gitignore

---
+++ index.js
@@ -0,0 +1,8 @@
+// import packages needed fro this application
+import fs from "fs"
+import inquirer from "inquirer"
+import { questions } from "./utilities/questions.js"
+import { generateMarkdown} from "./utilities/generateMarkdown.js"
+
+// create a function to write README file
+function writeToFile(fileName, data) {}
